12ccf5ee45e7f66593137d6bf90e295d77f29131
Eugeny Karpov <evgenii.karpov@leongaming.com>
2022-01-17 00:37:01 +0300

    add artifact

---
+++ docs/pkg/estonian_numbers_generator.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} number
+* @param {number} question
+* @returns {string}
+*/
+export function to_letters_js(number: string, question: number): string;
+/**
+*/
+export enum Question {
+  KuiPalju,
+  KuiVana,
+  Mitmes,
+  Mitmendal,
+}
---
+++ docs/pkg/estonian_numbers_generator.js
@@ -0,0 +1,2 @@
+import * as wasm from "./estonian_numbers_generator_bg.wasm";
+export * from "./estonian_numbers_generator_bg.js";
