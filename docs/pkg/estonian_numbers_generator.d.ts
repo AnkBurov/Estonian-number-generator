cf666e8dc2abaa71d9387423db55374750f2c14c
Eugeny Karpov <evgenii.karpov@leongaming.com>
2022-01-17 12:05:25 +0300

    update binaries

--- docs/pkg/estonian_numbers_generator.d.ts
+++ docs/pkg/estonian_numbers_generator.d.ts
@@ -14,3 +14,24 @@ export enum Question {
   Mitmes,
   Mitmendal,
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly to_letters_js: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
